d415347b3883cd1d177ad2e7b4d358b3c2417d59
thiagoferreiralucas <78644490+thiagoferreiralucas@users.noreply.github.com>
2023-08-14 16:05:34 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <template>
   <div>
+
+    <!-- Comentários -->
     <h1>Bem-Vindo a página do {{ value }}, espero que tenha gostado!</h1>
     <p>teste</p>
 
-    <h2>Tags de Texto</h2>
+    <hr>
+
+    <h2>Tags para Parágrafo</h2>
+
+      <!-- Quando menor o valor maior a importância -->
+      <h1>Cabeçalho Principal</h1>
+      <h2>Cabeçalho de Nível 2</h2>
+      <h3>Cabeçalho de Nível 3</h3>
+      <h4>Cabeçalho de Nível 4</h4>
+      <h5>Cabeçalho de Nível 5</h5>
+      <h6>Cabeçalho de Nível 6</h6>
+
+    <hr>
+
+    <h2>Tags para Texto</h2>
 
       <h3>Principais termos do glossário de Cloud Computing</h3>
   
@@ -18,18 +34,27 @@
 
     <hr>
 
-      <h3>Principais termos do glossário de Cloud Computing</h3>
-
-      <dl>
-          <dt>Baas</dt>
-          <dd>Backend as a Service</dd>
-          <dt>Iaas</dt>
-          <dd>Infrastructure as a Service</dd>
-          <dt>Paas</dt>
-          <dd>Platform as a Service</dd>
-          <dt>Saas</dt>
-          <dd>Software as a Service</dd>
-      </dl>
+    <h2>Listas</h2>
+    
+      <!-- Lista Ordenada ( ol - Order List | li - List Item ) -->
+      <h3>Aprovados</h3>
+      
+      <ol>
+          <li>Bia</li>
+          <li>Pedro</li>
+          <li>Ana</li>
+          <li>João</li>
+      </ol>
+      
+      <!-- Lista Desordenada ( ul - Unordered List ) -->
+      <h3>Lista de Compras</h3>
+      
+      <ul>
+          <li>Queijo</li>
+          <li>Arroz</li>
+          <li>Leite</li>
+          <li>Pão</li>
+      </ul>
 
     <hr>
 
