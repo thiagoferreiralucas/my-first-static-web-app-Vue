9bfbd7ef80fa40c12fefef08ba1d4be3e9424f54
thiagoferreiralucas <78644490+thiagoferreiralucas@users.noreply.github.com>
2023-08-14 16:29:02 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
 
     <hr>
 
-    <h2><s>Tags para Parágrafo</s></h2>
+    <h2><u>Tags para Parágrafo</u></h2>
 
       <!-- Quando menor o valor maior a importância -->
       <h1>Cabeçalho Principal</h1>
@@ -20,7 +20,7 @@
 
     <hr>
 
-    <h2><s>Tags para Texto</s></h2>
+    <h2><u>Tags para Texto</u></h2>
   
       <!-- Texto padrão -->
       <p>Texto qualquer</p>
@@ -29,14 +29,14 @@
       <p>Texto em <b>Negrito</b> e <i>Itálico</i></p  
                                                    
       <!-- Sublinado -->
-      <p>Texto <s>sublinhado</s></p>
+      <p>Texto <u>sublinhado</u></p>
       
       <!-- Sobrescrito e Subescrito -->
       <p>2<sup>2</sup> = 4 e H<sub>2</sub>O = Água!</p>
 
     <hr>
 
-    <h2><s>Tags para Listas</s></h2>
+    <h2><u>Tags para Listas</u></h2>
     
       <!-- Lista Ordenada ( ol - Order List | li - List Item ) -->
       <h3>Aprovados</h3>
@@ -60,13 +60,13 @@
 
     <hr>
 
-    <h2><s>Tags para Imagens</s></h2>
+    <h2><u>Tags para Imagens</u></h2>
     
     <img src="https://sorocaba.sp.senai.br/Img/logo-senai2.png">
 
     <hr>
 
-    <h2><s>Tags para Links</s></h2>
+    <h2><u>Tags para Links</u></h2>
     
     <a href="https://www.google.com.br" target="_black">Google</a>
 
