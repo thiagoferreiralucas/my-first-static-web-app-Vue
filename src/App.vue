1311e4203a3adc2177194b47dc8e5f9685c9d062
thiagoferreiralucas <78644490+thiagoferreiralucas@users.noreply.github.com>
2023-08-14 15:38:59 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,20 @@
   <div>
     <h1>Bem-Vindo a página do {{ value }}, espero que tenha gostado!</h1>
     <p>teste</p>
+
+    <h2>Principais termos do glossário de Cloud Computing</h2>
+
+    <dl>
+        <dt>Baas</dt>
+        <dd>Backend as a Service</dd>
+        <dt>Iaas</dt>
+        <dd>Infrastructure as a Service</dd>
+        <dt>Paas</dt>
+        <dd>Platform as a Service</dd>
+        <dt>Saas</dt>
+        <dd>Software as a Service</dd>
+    </dl>
+    
   </div>
 </template>
 
